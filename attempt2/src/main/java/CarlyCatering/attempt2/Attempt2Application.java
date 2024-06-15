7691a7419eb432969ea81a6ab59d133d9fda75ab
Typhene Sonia Benfriha <typh94@live.fr>
2024-06-15 13:09:09 -0500

    new attempt

---
+++ attempt2/src/main/java/CarlyCatering/attempt2/Attempt2Application.java
@@ -0,0 +1,13 @@
+package CarlyCatering.attempt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Attempt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Attempt2Application.class, args);
+	}
+
+}
---
+++ attempt2/src/test/java/CarlyCatering/attempt2/Attempt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package CarlyCatering.attempt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Attempt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
